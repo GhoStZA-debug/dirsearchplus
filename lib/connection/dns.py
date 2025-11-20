466fc467400118aec93a10fd149e4a23a6c7d9b4
codervibe <3217277135@qq.com>
2025-11-21 00:15:03 +0800

    feat(dns): 实现DNS解析结果缓存功能
    - 添加全局DNS缓存字典_dns_cache- 实现cache_dns函数用于手动缓存DNS解析结果
    - 实现cached_getaddrinfo函数作为socket.getaddrinfo的缓存替代品
    - 优化DNS查询性能，避免重复解析相同域名和端口
    - 添加函数文档字符串和注释提高代码可读性
    - 移除文件头部的GPL许可证声明和作者信息

--- lib/connection/dns.py
+++ lib/connection/dns.py
@@ -1,38 +1,30 @@
-# -*- coding: utf-8 -*-
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#  Author: Mauro Soria
-
 from socket import getaddrinfo
 
 _dns_cache = {}
 
 
 def cache_dns(domain, port, addr):
+    """
+    缓存DNS解析结果到全局缓存中
+
+    :param domain: 域名
+    :param port: 端口号
+    :param addr: 地址
+    """
     _dns_cache[domain, port] = getaddrinfo(addr, port)
 
 
 def cached_getaddrinfo(*args, **kwargs):
     """
-    Replacement for socket.getaddrinfo, they are the same but this function
-    does cache the answer to improve the performance
+    socket.getaddrinfo 的替代品，它们是一样的，但功能是这样
+    是否缓存答案以提升性能
     """
 
+    # 提取主机名和端口用于缓存键
     host, port = args[:2]
+    # 检查缓存中是否已存在该主机和端口的解析结果
     if (host, port) not in _dns_cache:
         _dns_cache[host, port] = getaddrinfo(*args, **kwargs)
 
     return _dns_cache[host, port]
+
