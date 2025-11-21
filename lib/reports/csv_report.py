bdd6dd9c4c0cc8354f96d0323b4c4b77851b1d04
codervibe <3217277135@qq.com>
2025-11-21 11:10:40 +0800

    feat(reports): 添加CSV报告生成器类
    - 实现CSVReport类继承自FileBaseReport
    - 添加get_header方法返回CSV头部信息- 实现generate方法生成完整的CSV格式报告
    - 支持对扫描结果条目的URL、状态码、大小、内容类型和重定向信息进行CSV序列化- 添加对重定向字段的CSV转义处理
    - 增加类和方法文档字符串，提升代码可读性

--- lib/reports/csv_report.py
+++ lib/reports/csv_report.py
@@ -1,39 +1,45 @@
-# -*- coding: utf-8 -*-
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#  Author: Mauro Soria
-
 from lib.core.settings import NEW_LINE
 from lib.reports.base import FileBaseReport
 from lib.utils.common import escape_csv
 
 
 class CSVReport(FileBaseReport):
+    """
+    CSV报告生成器类
+
+    继承自FileBaseReport，用于生成CSV格式的扫描结果报告
+    """
+
     def get_header(self):
+        """
+        获取CSV文件头部信息
+
+        返回值:
+            str: 包含列名的CSV头部字符串，以换行符结尾
+        """
         return "URL,Status,Size,Content Type,Redirection" + NEW_LINE
 
     def generate(self, entries):
+        """
+        生成CSV格式的报告内容
+
+        参数:
+            entries: 扫描结果条目列表，每个条目应包含url、status、length、type和redirect属性
+
+        返回值:
+            str: 完整的CSV格式报告字符串
+        """
         output = self.get_header()
 
+        # 遍历所有扫描结果条目，生成CSV行数据
         for entry in entries:
             output += f"{entry.url},{entry.status},{entry.length},{entry.type},"
 
+            # 如果存在重定向信息，则对其进行CSV转义处理后添加到输出
             if entry.redirect:
                 output += f'"{escape_csv(entry.redirect)}"'
 
             output += NEW_LINE
 
         return output
+
